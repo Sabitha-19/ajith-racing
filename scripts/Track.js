525c1fd79f153328556698bdedb47ce7121169c2
Sabitha-19 <sabithas7049@gmail.com>
2025-11-21 20:25:23 +0530

    Update Track.js

--- scripts/Track.js
+++ scripts/Track.js
@@ -1,65 +1,27 @@
-// Track.js
 export default class Track {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+    constructor() {
+        this.segments = [];
 
-        // Road properties
-        this.roadWidth = this.width * 0.6;
-        this.laneCount = 3;
-
-        // Generate lane boundaries
-        this.leftBorder = (this.width - this.roadWidth) / 2;
-        this.rightBorder = this.leftBorder + this.roadWidth;
-
-        // For scrolling effect
-        this.offsetY = 0;
-        this.speed = 5;
-    }
-
-    update(delta) {
-        this.offsetY += this.speed * delta;
-        if (this.offsetY > this.height) {
-            this.offsetY = 0;
+        for (let i = 0; i < 200; i++) {
+            this.segments.push({
+                x: Math.sin(i * 0.1) * 10,
+                y: -(i * 5),
+                width: 15
+            });
         }
     }
 
-    draw(ctx) {
+    draw(ctx, camera) {
         ctx.fillStyle = "#222";
-        ctx.fillRect(0, 0, this.width, this.height);
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        // Road background
-        ctx.fillStyle = "#333";
-        ctx.fillRect(this.leftBorder, 0, this.roadWidth, this.height);
+        ctx.fillStyle = "#555";
 
-        // Lane lines
-        ctx.strokeStyle = "#fff";
-        ctx.lineWidth = 4;
-        ctx.setLineDash([30, 30]);
+        for (let seg of this.segments) {
+            let sx = seg.x - camera.x + ctx.canvas.width / 2;
+            let sy = seg.y - camera.y + ctx.canvas.height;
 
-        for (let i = 1; i < this.laneCount; i++) {
-            const x = this.getLaneCenter(i) - this.roadWidth / this.laneCount / 2;
-            ctx.beginPath();
-            ctx.moveTo(x, -this.offsetY);
-            ctx.lineTo(x, this.height - this.offsetY);
-            ctx.stroke();
-        }
-
-        ctx.setLineDash([]);
-    }
-
-    getLaneCenter(laneIndex) {
-        const laneWidth = this.roadWidth / this.laneCount;
-        return this.leftBorder + laneWidth / 2 + laneIndex * laneWidth;
-    }
-
-    inRoad(x) {
-        return x >= this.leftBorder && x <= this.rightBorder;
-    }
-
-    checkCollision(player) {
-        if (!this.inRoad(player.x)) {
-            player.crash = true;
+            ctx.fillRect(sx - seg.width, sy, seg.width * 2, 10);
         }
     }
 }
